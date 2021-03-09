731d16e17b135a9eebe07db00eb98875a9a5b907
Jitka <jm.works.online@gmail.com>
2021-03-09 13:58:16 +0100

    add arrow icon rotation

--- app.js
+++ app.js
@@ -3,8 +3,11 @@ const accordions = document.querySelectorAll('.accordion');
 accordions.forEach(function(accordion){
  accordion.addEventListener('click', function(){
        accordion.classList.toggle('active');
-       var panel = accordion.nextElementSibling
-       console.log(panel.innerHTML);
+       var panel = accordion.nextElementSibling;
+       
+       var arrow = accordion.childNodes;
+       arrow[3].classList.toggle('arrow--active');
+       console.log(arrow[3].classList);
        if(panel.style.maxHeight){
           panel.style.maxHeight = null;
        }else{
